6506d472a8b9a14a454399725783fd6791a1c73f
MrPoifect <106980585+MrPoifect@users.noreply.github.com>
2026-04-21 21:56:45 +0100

    add Grid and highlight
    grid generates correctly
    mouse overlapping highlight current target

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector("#container");
+const defaultColour = "rgb(185, 185, 185)";
+let paintColour = "rgb(243, 0, 0)";
+
+function createGrid(size) {
+    for(i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+
+        for (j = 0; j < size; j++) {
+            const column = document.createElement("div");
+            column.classList.add("box", "column");
+            row.appendChild(column);
+            column.style.backgroundColor = defaultColour;
+            column.addEventListener("mouseenter", function(){
+                setGridColour(column, paintColour)
+            });
+            column.addEventListener("mouseleave", function(){
+                revertGridColour(column)
+            });
+        }
+    }
+    
+}
+
+createGrid(16);
+
+function setGridColour(box, colour) {
+    box.style.backgroundColor = paintColour;
+}
+
+function revertGridColour(box) {
+    box.style.backgroundColor = defaultColour;
+}
